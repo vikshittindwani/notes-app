7e136ce5a1fb3fd95a3491dca2fa75542987e6c8
vikshittindwani <vickytindwani366@gmail.com>
2025-09-05 13:02:44 +0530

    Update test_app.py

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -9,4 +9,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json["message"] == "Hello, CI/CD,vicky!"
+    assert response.json["message"] == "Hello, CI/CD, vicky!"
