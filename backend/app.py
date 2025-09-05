504bbb8ffb53fa41a2067396e4e91b05e9804362
vikshittindwani <vickytindwani366@gmail.com>
2025-09-05 12:55:09 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello, CI/CD!"})
+    return jsonify({"message": "Hello, CI/CD, vicky!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
